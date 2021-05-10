f2f710f358fd8037b4382c27eca6285b51be1f1e
dreamSeekerYu <yupeng.bai@ucarinc.com>
2021-05-10 17:24:55 +0800

    start

---
+++ nuxt.config.js
@@ -0,0 +1,75 @@
+export default {
+  // Global page headers: https://go.nuxtjs.dev/config-head
+  head: {
+    title: 'nuxt-app',
+    htmlAttrs: {
+      lang: 'en'
+    },
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: '' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+
+  // Global CSS: https://go.nuxtjs.dev/config-css
+  css: [
+    'element-ui/lib/theme-chalk/index.css'
+  ],
+
+  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+  plugins: [
+    '@/plugins/element-ui'
+  ],
+
+  // Auto import components: https://go.nuxtjs.dev/config-components
+  components: true,
+
+  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+  buildModules: [
+  ],
+
+  // Modules: https://go.nuxtjs.dev/config-modules
+  modules: [
+    '@nuxtjs/axios',
+    '@nuxtjs/proxy'
+  ],
+
+  // Build Configuration: https://go.nuxtjs.dev/config-build
+  build: {
+    transpile: [/^element-ui/],
+    vendor: ['axios']
+  },
+  axios: {
+    proxy: true,
+    prefix: '/api', // baseURL
+    changOrigin: true,
+    credentials: true,
+  },
+  // proxy: {
+  //   '/api/**': {
+  //     target: 'http://81.70.36.99:8001', // 代理地址
+  //     // target: 'https://www.quanqingkeji.com/', // 代理地址
+  //     changeOrigin: true,
+  //     pathRewrite: {
+  //       '^/api': ''
+  //     },
+  //   },
+  // },
+  proxy: [
+    ['/api', {
+      target: 'http://81.70.36.99:8001',
+      changOrigin: true,
+      pathRewrite: {
+        '^/api': ''
+      },
+    }]
+  ],
+  server: {
+    port: 80, // default: 3000
+    // host: '0.0.0.0' // default: localhost,
+  }
+}
---
+++ pages/index.vue
@@ -0,0 +1,80 @@
+<template>
+  <div class="container">
+    <div>
+      <Logo />
+      <h1 class="title">nuxt-app</h1>
+      <div class="links">
+        <a
+          href="https://nuxtjs.org/"
+          target="_blank"
+          rel="noopener noreferrer"
+          class="button--green"
+        >
+          Documentation
+        </a>
+        <a
+          href="https://github.com/nuxt/nuxt.js"
+          target="_blank"
+          rel="noopener noreferrer"
+          class="button--grey"
+        >
+          GitHub
+        </a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import request from "@/plugins/service";
+export default {
+  async mounted() {
+    // const res = await axios.get("https://www.quanqingkeji.com/api/area/list");
+    // axios.header;
+    const res = await request.get("/api/area/list");
+
+    console.log(
+      "%c res....",
+      "color: red; font-style: italic",
+      JSON.stringify(res.data, null, 2)
+    );
+  },
+  // async asyncData({ params }) {
+  //   let { data } = await axios.get("https://www.quanqingkeji.com/api/area/list");
+  //   return { title: data.title };
+  // },
+};
+</script>
+
+<style>
+.container {
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+.title {
+  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont,
+    "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif;
+  display: block;
+  font-weight: 300;
+  font-size: 100px;
+  color: #35495e;
+  letter-spacing: 1px;
+}
+
+.subtitle {
+  font-weight: 300;
+  font-size: 42px;
+  color: #526488;
+  word-spacing: 5px;
+  padding-bottom: 15px;
+}
+
+.links {
+  padding-top: 15px;
+}
+</style>
